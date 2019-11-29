71ef8ecc2e2b59816df237c9e8ab70296be4b5f5
alerighi <alerighi4@gmail.com>
2019-11-29 17:48:00 +0100

    Reformat code

--- src/tests/limits.rs
+++ src/tests/limits.rs
@@ -1,8 +1,9 @@
-use super::util::*;
-use crate::{ExitStatus, SandboxConfigurationBuilder};
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use super::util::*;
+use crate::{ExitStatus, SandboxConfigurationBuilder};
 
 #[test]
 fn test_memory_limit_ok() {
