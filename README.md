bbe300e20f60b9737f31908fcc283d7a7a1a6406
alerighi <alerighi4@gmail.com>
2019-12-01 09:59:29 +0100

    Update license header

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+<!-- 
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+SPDX-License-Identifier: MPL-2.0
+-->
+
 # tabox
 
 [![Docs]( https://docs.rs/tabox/badge.svg)]( https://docs.rs/tabox)
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
 
 //! [![Docs]( https://docs.rs/tmbox/badge.svg)]( https://docs.rs/tmbox)
 //! [![crates.io](https://img.shields.io/crates/v/tabox.svg)](https://crates.io/crates/tabox)
